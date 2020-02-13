c39b55e1464c2f8c781750826155b9d79fbeacfa
Matt Davey <matthew.davey@res-group.com>
2020-02-13 14:42:39 +0000

    linked test and main code. test now running

--- tests/test_solution_calculator.py
+++ tests/test_solution_calculator.py
@@ -1,5 +1,6 @@
+import problem_sovler.solution_calculator as sc
 
 def test_should_return_answer_when_the_solution_is_requested():
-    solution_calculator = SolutionCalculator()
-    expected_solution = solution_calculator.calculate(7)
+    solution_calculator = sc.SolutionCalculator(7)
+    expected_solution = solution_calculator.calculate()
     assert expected_solution == 7

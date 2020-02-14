118438a938de06a7c2b04044ade174037d5ad09d
Matt Davey <matthew.davey@res-group.com>
2020-02-14 11:42:49 +0000

    implemented 1 hour 5 min and 1 min lights and tests

--- berlin_clock/light_calculator.py
+++ berlin_clock/light_calculator.py
@@ -5,23 +5,49 @@ class BerlinClock:
 	def __init__(self,time_string):
 		time_format = '%H:%M:%S'
 		self.date_time = dt.datetime.strptime(time_string,time_format)
-		self.row_1_light = self.calculate_row_1_light()
-		self.row_2_lights = self.calculate_row_2_lights()
+		self.seconds_light = self.calculate_seconds_light()
+		self.five_hour_lights = self.calculate_5_hour_lights()
+		self.one_hour_lights = self.calculate_1_hour_lights()
+		self.five_minute_lights = self.calculate_5_minute_lights()
+		self.one_minute_lights = self.calculate_1_minute_lights()
 
-	def calculate_row_1_light(self):
+	def calculate_seconds_light(self):
 		if (self.date_time.second % 2) == 0:
-			print("{0} is Even".format(self.date_time.second))
-			light_state = 'Y'
-		elif (self.date_time.second % 2) == 1:
-			print("{0} is Odd".format(self.date_time.second))
-			light_state = 'O'
-		return light_state
+			return 'Y'
+		return 'O'
 
-	def calculate_row_2_lights(self):
+	def calculate_5_hour_lights(self):
 		full_hours = int(self.date_time.hour/5)
 		light_string = ''
 		for i in range(0,full_hours):
 			light_string += 'R'
+		while len(light_string) < 4:
+			light_string += 'O'
+		return light_string
+
+	def calculate_1_hour_lights(self):
+		remainder_hours = int(self.date_time.hour % 5)
+		light_string = ''
+		for i in range(0,remainder_hours):
+			light_string += 'R'
+		while len(light_string) < 4:
+			light_string += 'O'
+		return light_string
+
+	def calculate_5_minute_lights(self):
+		full_five_minutes = int(self.date_time.minute / 5)
+		light_string = ''
+		for i in range(0,full_five_minutes):
+			light_string += 'Y'
+		while len(light_string) < 11:
+			light_string += 'O'
+		return light_string
+
+	def calculate_1_minute_lights(self):
+		remainder_minutes = int(self.date_time.minute % 5)
+		light_string = ''
+		for i in range(0,remainder_minutes):
+			light_string += 'Y'
 		while len(light_string) < 4:
 			light_string += 'O'
 		return light_string
--- tests/test_solution_calculator.py
+++ tests/test_solution_calculator.py
@@ -4,10 +4,26 @@ testtime = '12:56:01'
 
 def test_should_return_O_when_seconds_are_odd(testtime = '12:56:01'):
     clock = berlin_clock.light_calculator.BerlinClock(testtime)
-    expected_solution = clock.calculate_row_1_light()
+    expected_solution = clock.calculate_seconds_light()
     assert expected_solution == 'O'
 
 def test_should_return_RROO_when_2x_full_5_hours(testtime = '12:56:01'):
     clock = berlin_clock.light_calculator.BerlinClock(testtime)
-    expected_solution = clock.calculate_row_2_lights()
+    expected_solution = clock.calculate_5_hour_lights()
     assert expected_solution == 'RROO'
+
+def test_should_return_RROO_when_2x_full_1_hours_remainder(testtime = '12:56:01'):
+    clock = berlin_clock.light_calculator.BerlinClock(testtime)
+    expected_solution = clock.calculate_1_hour_lights()
+    assert expected_solution == 'RROO'
+
+def test_should_return_YYYYYYYYYYY_when_11x_5_minutes(testtime = '12:56:01'):
+    clock = berlin_clock.light_calculator.BerlinClock(testtime)
+    expected_solution = clock.calculate_5_minute_lights()
+    assert expected_solution == 'YYYYYYYYYYY'
+
+
+def test_should_return_YOOO_when_1x_1_minutes_remainder(testtime = '12:56:01'):
+    clock = berlin_clock.light_calculator.BerlinClock(testtime)
+    expected_solution = clock.calculate_1_minute_lights()
+    assert expected_solution == 'YOOO'

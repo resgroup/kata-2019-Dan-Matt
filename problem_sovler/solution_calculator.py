e8a89e40f589737e0fbdf8d7d8d1be747c507db5
Matt Davey <matthew.davey@res-group.com>
2020-02-14 09:46:30 +0000

    Added test for odd

--- problem_sovler/solution_calculator.py
+++ problem_sovler/solution_calculator.py
@@ -1,9 +1,18 @@
 import numpy as np
+import datetime as dt
 
-class SolutionCalculator:
-	def __init__(self,input):
-		self.input = input
+class BerlinClock:
+	def __init__(self,time_string):
+		time_format = '%H:%M:%s'
+		self.date_time = dt.datetime.strptime(time_string,time_format)
 
-	def calculate(self):
-		answer = np.nan
-		return answer
+	def calculate_row_1_light(self):
+		seconds = self.date_time.second()
+		if (seconds % 2) == 0:
+			print("{0} is Even".format(seconds))
+			light_state = 'Y'
+		else:
+			print("{0} is Odd".format(seconds))
+			light_state = 'O'
+
+		return light_state
--- tests/test_solution_calculator.py
+++ tests/test_solution_calculator.py
@@ -1,6 +1,13 @@
 import problem_sovler.solution_calculator as sc
 
+testtime = '12:56:01'
+
 def test_should_return_answer_when_the_solution_is_requested():
-    solution_calculator = sc.SolutionCalculator(7)
+    solution_calculator = sc.BerlinClock(7)
     expected_solution = solution_calculator.calculate()
     assert expected_solution == 7
+
+def test_should_return_O_when_seconds_are_odd(testtime):
+    solution_calculator = sc.BerlinClock(testtime)
+    expected_solution = solution_calculator.calculate()
+    assert expected_solution == 'O'
